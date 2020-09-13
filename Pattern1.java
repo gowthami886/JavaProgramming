22525f44cbe482099cd75f9e0cf4616fabed9bea
Gowthami Rajendra <57149012+gowthami886@users.noreply.github.com>
2020-09-13 17:19:38 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -5,12 +5,12 @@ class Solution {
 	Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int i, j;
-	    char c = (char)('A' + n - 1);
+	char c = (char)('A' + n - 1);
         for(i = 1; i <= n; i += 1)
         {
             for(j = 1; j <= i; j += 1)
             {
-		        System.out.print((char)(c - i + j));
+		System.out.print((char)(c - i + j));
             }
             System.out.println();
         }
