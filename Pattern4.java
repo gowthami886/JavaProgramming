8a328b7c59fd5a676b57576a36b55fd954658e05
Gowthami Rajendra <57149012+gowthami886@users.noreply.github.com>
2020-09-13 17:47:51 +0530

    Character Pattern

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1, j;
+        while(i <= n)
+        {	
+     				char c = (char)('A' + i -1);
+        		j = 1;
+           		while(j <= i)
+            	{
+                System.out.print(c);
+                c = (char)(c + 1);
+                j+= 1;
+           	 }
+           System.out.println();
+           i+= 1;
+        }
+		
+	}
+
+}
